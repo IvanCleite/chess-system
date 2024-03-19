a3e7f5b181378d3e9238b56ab585edc330012601
IvanCleite <ivancarvalholeite@gmail.com>
2024-03-19 10:29:27 -0300

    Colocando as peças no tabuleiro

--- src/Program.java
+++ src/Program.java
@@ -5,7 +5,6 @@ public class Program {
     public static void main(String[] args) {
         
        ChessMatch chessMatch = new ChessMatch();
-       System.out.println();
        UI.printBoard(chessMatch.getPieces());
 
 
